2b0bb48d0938f183ef821c12fbece43fca04e09c
lipeng0820 <lipeng0820@qq.com>
2023-03-24 03:40:57 +0800

    0.0.2版本
    还是存在间歇失效的问题，不如0.0.1版简单粗暴行之有效。

---
+++ newBingHelper/content.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <style>
+      button {
+        font-size: 14px;
+        width: 200px;
+        height: 30px;
+      }
+    </style>
+  </head>
+  <body>
+    <button id="callHelper">Hi!newBing</button>
+    <script src="popup.js"></script>
+  </body>
+</html>
