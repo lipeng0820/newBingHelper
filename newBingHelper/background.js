6c8054f14a4a37b66dac1a82f76e3edc55aefcba
lipeng0820 <lipeng0820@qq.com>
2023-03-24 03:42:16 +0800

    0.0.1初始版本
    纯框架，但成功率高，好用。可惜了不能改地址。万一官方逻辑变了就废了。

--- newBingHelper/background.js
+++ newBingHelper/background.js
@@ -1,15 +1,10 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "callNewBingHelper") {
-    chrome.storage.local.get(['nBh_region', 'nBh_language'], (result) => {
-      const nBh_region = result.nBh_region || "https://www.bing.com/account/action?scope=web&cc=us&FORM=W5WA&uid=E2313290&sid=145E3F849DBD68661CFA2D589C936941&ru=%2f%3fform%3dREDIRERR&settingpage=1";
-      const nBh_language = result.nBh_language || "https://www.bing.com/account/action?scope=web&setlang=zh-Hans&FORM=W5WA&uid=E2313290&sid=145E3F849DBD68661CFA2D589C936941&ru=%2f%3fform%3dREDIRERR%26cc%3dus&settingpage=1&ntb=1";
-
-      Promise.all([
-        fetch(nBh_region),
-        fetch(nBh_language)
-      ]).then(() => {
-        chrome.tabs.update({ url: "https://chat.bing.com" });
-      });
+    Promise.all([
+      fetch("https://www.bing.com/account/action?scope=web&cc=us&FORM=W5WA&uid=E2313290&sid=145E3F849DBD68661CFA2D589C936941&ru=%2f%3fform%3dREDIRERR&settingpage=1"),
+      fetch("https://www.bing.com/account/action?scope=web&setlang=zh-Hans&FORM=W5WA&uid=E2313290&sid=145E3F849DBD68661CFA2D589C936941&ru=%2f%3fform%3dREDIRERR%26cc%3dus&settingpage=1&ntb=1")
+    ]).then(() => {
+      chrome.tabs.update({ url: "https://chat.bing.com" });
     });
   }
 });
